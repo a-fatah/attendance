7394cb87a4736f9ca937bd889c9556e74c3572d5
Abdul Fatah <abdulfatah66@gmail.com>
2021-03-10 09:37:17 +0500

    fetch names from random user api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  useEffect(() => {
+    fetch("https://randomuser.me/api?results=10")
+      .then((res) => res.json())
+      .then((json) => json.results)
+      .then((people) => people.map((person) => person.name.first))
+      .then((names) => console.log(names));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="text-center">Beacon House School System</h1>
+      <h3 className="text-center">Attendance Roster</h3>
     </div>
   );
 }
